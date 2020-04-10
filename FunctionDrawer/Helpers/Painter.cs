68af1e2f2fcbf587b2bb190b3594a5b1d1224ed9
Loghin Alexandru <a.loghin@levi9.com>
2020-04-10 16:24:19 +0300

    Add initial commit

---
+++ FunctionDrawer/Helpers/Painter.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+
+namespace FunctionPlotter.Helpers
+{
+    public sealed class Painter
+    {
+        private readonly Graphics _graphics;
+        private readonly Bitmap _bitmap;
+
+        public Painter()
+        {
+            _bitmap = new Bitmap(1024, 1024);
+            _graphics = Graphics.FromImage(_bitmap);
+        }
+
+        public void DrawFunction(List<Func<double, double>> function, double low, double high, double stepSize)
+        {
+            for (var range = low; range < high; range += stepSize)
+            {
+                var resultY = 0.0;
+                resultY += function.Sum(entry => entry(range));
+                DrawPoint(-(int)range + _bitmap.Width / 2, -(int)resultY + _bitmap.Height / 2);
+            }
+        }
+
+        public Bitmap GetBitmap()
+        {
+            return _bitmap;
+        }
+
+        private void DrawPoint(int positionX, int positionY)
+        {
+            _graphics.FillRectangle(Brushes.Red, positionX, positionY, 1, 1);
+        }
+    }
+}
---
+++ FunctionDrawer/MainWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using FunctionPlotter.Helpers;
+
+namespace FunctionDrawer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private readonly Painter _painter;
+        private readonly ExpressionParser _parser;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            _painter = new Painter();
+            _parser = new ExpressionParser();
+        }
+
+        private void InputEntered(object sender, TextChangedEventArgs e)
+        {
+            var function = _parser.Parse(Input.Text);
+
+            _painter.DrawFunction(function, -100,100,0.001);
+
+            FunctionImage.Source = Converters.BitmapToImageSource(_painter.GetBitmap());
+        }
+    }
+}
